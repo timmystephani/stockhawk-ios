b6ddc8e0a0732735b494970343e5a308838172cb
Tim Stephani - Mac Laptop <timmy.stephani@gmail.com>
2015-08-30 21:07:04 -0700

    remove mike from info, add new icon assets

--- StockHawk/InfoViewController.swift
+++ StockHawk/InfoViewController.swift
@@ -20,8 +20,8 @@ class InfoViewController: UIViewController {
         infoLabel.text =
             "Instructions:\nPull the list of stocks down to refresh\nSwipe a stock left to delete\n\n" +
             "StockHawk uses the Yahoo! Finance API for stock quotes.\n\n" +
-            "Questions or comments? I'd love to hear them! Please send me an email at stockhawkcontact@gmail.com\n\n" +
-            "Special thanks to Mike Winkelmann (beeple-crap.com) for designing the app icon."
+            "Questions or comments? I'd love to hear them! Please send me an email at stockhawkcontact@gmail.com" //"\n\n" +
+            //"Special thanks to Mike Winkelmann (beeple-crap.com) for designing the app icon."
 
     }
     @IBAction func done() {
